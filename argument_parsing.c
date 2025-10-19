d3998f9981be3fb74870b5a6f2d8be8b47bb232f
antabord <antabord@student.42lisboa.com>
2025-10-19 11:26:27 +0100

    Planing_next_steps

---
+++ argument_parsing.c
@@ -0,0 +1,6 @@
+#include "include.h"
+
+int	argument_parsing(char *argv[])
+{
+	
+}
---
+++ pipex.c
@@ -0,0 +1,10 @@
+#include "include.h"
+
+int	main(int argc, char *argv[], char **env)
+{
+	if (argc < 4)
+		return (0);
+	argument_parsing(argv);
+	
+
+}
